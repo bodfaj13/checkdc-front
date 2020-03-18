26431c6c4dc12ab4a59d8a949ee82b778ddec5e1
Bello Ajibola <bellohargbola13@gmail.com>
2020-03-18 07:46:25 +0100

    set preloader

--- src/pages/homepage/homepage.jsx
+++ src/pages/homepage/homepage.jsx
@@ -4,37 +4,51 @@ import LoginForm from '../../components/login/loginform'
 import SignupForm from '../../components/signup/signupform'
 import { connect } from 'react-redux'
 import { loginUser } from '../../core/actions/userActions'
+import LoadingOutlined from '@ant-design/icons/LoadingOutlined';
 import './homepage.scss'
 
 class Homepage extends Component {
   constructor(props) {
     super(props)
     this.state = {
-      showLogin: true
+      showLogin: true,
+      preload: true
     }
   }
 
+  componentDidMount() {
+    setTimeout(() => {
+      this.setState({
+        preload: false
+      })
+    }, 1500);
+  }
+
   toggleGateway = () => {
-    const { showLogin } = this.state 
+    const { showLogin } = this.state
     this.setState({
       showLogin: !showLogin
     })
   }
 
   render() {
-    const { showLogin } = this.state
+    const { showLogin, preload } = this.state
     return (
-      <div className="homapage">
-        <div className="homapage-main">
-          <img src="/img/drug.svg" alt="" className="app-logo"/>
-          <h2 className="app-name"> Prescription Reminder</h2>
-          <Card className="gateway">
-            {
-              showLogin ? <LoginForm toggleGateway={this.toggleGateway} loginUser={this.props.loginUser} history={this.props.history}/> : <SignupForm toggleGateway={this.toggleGateway}/>
-            }
-          </Card>
+      preload ?
+        <div className="x-loader">
+          <LoadingOutlined />
+        </div> :
+        <div className="homapage">
+          <div className="homapage-main">
+            <img src="/img/drug.svg" alt="" className="app-logo" />
+            <h2 className="app-name"> Prescription Reminder</h2>
+            <Card className="gateway">
+              {
+                showLogin ? <LoginForm toggleGateway={this.toggleGateway} loginUser={this.props.loginUser} history={this.props.history} /> : <SignupForm toggleGateway={this.toggleGateway} />
+              }
+            </Card>
+          </div>
         </div>
-      </div>
     )
   }
 }
